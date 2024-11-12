10892775fcf36321e0f1be2e6b834c832bfcb684
Takolberry <takolberry@hotmail.com>
2024-11-12 20:20:56 -0300

    Coloquei corpo do produto

--- src/main/java/com/belval/time/entity/Produto.java
+++ src/main/java/com/belval/time/entity/Produto.java
@@ -1,5 +1,123 @@
 package com.belval.time.entity;
 
-public class Produto {
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto {
+	
+	//Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_produto") 
+	private Integer id;
+	
+	@Column(name = "nm_produto")
+	private String nome;
+	
+	@Column(name = "ds_produto")
+	private String descricao;
+	
+	@Column(name = "preco_produto")
+	private double preco;
+	
+	@Column(name = "data_criacao_produto", columnDefinition = "DATETIME")
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm:ss")
+	private LocalDateTime dataCriacao;
+	
+	//Método construtor padrão, isto é, sem parâmetros
+	public Produto() {
+		
+	}
+
+	//Alt + SHIFT + S > Generate contructor using Fields
+	public Produto(Integer id, String nome, String descricao, double preco, LocalDateTime dataCriacao) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataCriacao, descricao, id, nome, preco);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(dataCriacao, other.dataCriacao) && Objects.equals(descricao, other.descricao)
+				&& Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
+				&& Double.doubleToLongBits(preco) == Double.doubleToLongBits(other.preco);
+	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco=" + preco
+				+ ", dataCriacao=" + sdf.format(dataCriacao) + "]";
+	}
+
+	
+	
 }
